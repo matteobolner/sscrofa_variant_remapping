0922f2e776046f841e507702d940359daa558621
matteo.bolner <matteo.bolner@studio.unibo.it>
2020-12-15 13:27:06 +0100

    updates

--- scripts/update_vcf.py
+++ scripts/update_vcf.py
@@ -6,7 +6,7 @@ remapped_df = snakemake.input[1]
 #input_vcf = "/home/pelmo/sscrofa_variant_remapping/data/vcf_files/whole_POP_all.vcf"
 df = pd.read_csv(input_vcf, sep = "\t", comment = '#', header = None)
 remapped_df = pd.read_csv(remapped_df)
-remapped_df = remapped_df[['gene_name_10', 'ensembl_id_10', 'ensembl_id_11', 'chr_11_ensembl', 'strand_11', 'gene_start_11', 'gene_end_11', 'rel_var_pos_10', 'rel_var_pos_11', 'var_pos_11']].reset_index()
+remapped_df = remapped_df[['gene_name_10', 'ensembl_id_10', 'ensembl_id_11', 'chr_11_ensembl', 'strand_11', 'gene_start_11', 'gene_end_11', 'rel_var_pos_10', 'rel_var_pos_11', 'var_pos_11']].reset_index(drop=True)
 remapped_df['id_pos'] = remapped_df['ensembl_id_10'] + "_" + remapped_df['rel_var_pos_10'].astype(str)
 df['id_pos'] = df[0].str.split('|').str[1] + "_" + df[1].astype(str)
 merged = df.merge(remapped_df, left_on='id_pos', right_on='id_pos')
